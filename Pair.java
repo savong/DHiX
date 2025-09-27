36d66c0418236f90154919562b42d5b24fd095c6
Savong Bou <38307823+savong@users.noreply.github.com>
2025-09-27 13:23:09 +0900

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,3 +1,8 @@
+/*
+This is the implementation of the paper "DHiX: Dynamic and Hybrid Real-time Window Aggregations with Constant Time Complexity".
+By Savong Bou, Toshiyuki Amagasa, Hiroyuki Kitagawa
+*/
+
 // Utility Pair class if not using external libraries
 class Pair<K, V> {
     private K key;
