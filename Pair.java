61f14f97392abca928e5919ce1b9e817b1c8b358
Savong Bou <38307823+savong@users.noreply.github.com>
2025-09-27 12:19:52 +0900

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,8 @@
+// Utility Pair class if not using external libraries
+class Pair<K, V> {
+    private K key;
+    private V value;
+    public Pair(K k, V v) { key = k; value = v; }
+    public K getKey() { return key; }
+    public V getValue() { return value; }
+}
